283af9a71b2df8a98f5d3852ff60b639cd7b6a5b
Siva Surya Anagani <adattaramasivasurya17029@gmail.com>
2023-06-22 13:47:12 +0530

    folders created

---
+++ module1/day1/L1/1.c
@@ -0,0 +1,21 @@
+//Functions for determining the larger of two numbers
+
+//using if-else
+
+int findLarger(int number1, int number2) {
+if (number1 > number2) {
+return number1;
+} else {
+return number2;
+}
+}
+
+//Using the ternary operator:
+
+int findLarger(int num1, int num2) {
+return (num1 > num2) ? num1 : num2;
+}
+
+
+
+
